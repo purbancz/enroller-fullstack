db46f2cdd9dabba35e76936d9cf55fb396d83169
student17 <student17@pcoip.ki.agh.edu.pl>
2019-04-28 12:17:12 +0200

    authentication added

--- src/main/java/com/company/enroller/security/WebSecurity.java
+++ src/main/java/com/company/enroller/security/WebSecurity.java
@@ -1,18 +1,47 @@
 package com.company.enroller.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+	
+	@Value("${application.properties.security.secret}")
+	private String secret;
+	
+	@Value("${application.properties.security.issuer}")
+	private String issuer;
+	
+	@Value("${application.properties.security.token_expiration_in_seconds")
+	private int tokenExpiration;
+	
+	
+	@Autowired
+	ParticipantProvider participantProvider;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
    @Override
    protected void configure(HttpSecurity http) throws Exception {
        http.csrf().disable()
                .authorizeRequests()
                .anyRequest().permitAll()
                .and()
+//               .addFilterBefore(new JWTAuthenticationFilter(authenticationManager(), secret, issuer, tokenExpiration), UsernamePasswordAuthenticationFilter.class)
                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+   }
+   
+   @Override
+   public void configure(AuthenticationManagerBuilder auth) throws Exception {
+      auth.userDetailsService(participantProvider).passwordEncoder(passwordEncoder);
    }
 }
