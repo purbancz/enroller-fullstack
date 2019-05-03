90842036336348f5fb6c584d22813cb5b1739f42
Piotr Urbanczyk <p.m.urbanczyk@gmail.com>
2019-05-03 23:02:43 +0200

    cleaning after merging. Blah

--- src/main/frontend/src/main.js
+++ src/main/frontend/src/main.js
@@ -6,10 +6,6 @@ Vue.use(VueResource);
 
 Vue.http.options.root = '/api';
 
-import VueResource from "vue-resource";
-Vue.use(VueResource);
-Vue.http.options.root = '/api';
-
 new Vue({
     el: '#app',
     render: h => h(App)
